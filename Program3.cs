f293073d011e1efb643a8e7483c268d3370d7382
pashqa <pashqevi4@gmail.com>
2023-03-08 20:48:32 +0700

    task 3 finished

---
+++ Program3.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter your number");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+    Console.WriteLine("This number is even");
+else
+    Console.WriteLine("This number is odd");

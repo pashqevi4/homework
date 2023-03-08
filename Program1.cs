5d8251e0c134e83dce4c6104c57d4a519436a061
pashqa <pashqevi4@gmail.com>
2023-03-08 20:47:21 +0700

    task 1 finished

---
+++ Program1.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    int max = a;
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+if (a < b)
+{
+    int max = b;
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}

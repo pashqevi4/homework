b1272ff9f0aa4cd123c22e7c26a014ccb39f49b2
pashqa <pashqevi4@gmail.com>
2023-03-08 20:49:01 +0700

    task 4 finished

---
+++ Program4.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your number");
+int N = Convert.ToInt32(Console.ReadLine());
+int b = 1;;
+while (b < N)
+{
+    if (b % 2 == 0)
+        if (b == N - 1)
+            Console.Write($"{b}");
+        else
+            Console.Write($"{b}, ");
+    b++;
+}
